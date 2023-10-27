5b042a2bdaa8210f263de42c57bcc7344fbc2d5f
unknown <gzanabriaz28@gmail.com>
2023-10-27 11:32:08 -0500

    floodlight mactracker

---
+++ src/main/java/net/floodlightcontroller/pktinhistory/PktInHistory.java
@@ -0,0 +1,82 @@
+package net.floodlightcontroller.pktinhistory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.types.SwitchMessagePair;
+import net.floodlightcontroller.util.ConcurrentCircularBuffer;
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+public class PktInHistory implements IFloodlightModule, IOFMessageListener {
+    protected IFloodlightProviderService floodlightProvider;
+    protected ConcurrentCircularBuffer<SwitchMessagePair> buffer;
+    @Override
+    public String getName() {
+        return "PktInHistory";
+    }
+
+    @Override
+    public boolean isCallbackOrderingPrereq(OFType type, String name) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isCallbackOrderingPostreq(OFType type, String name) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public net.floodlightcontroller.core.IListener.Command receive(
+            IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+        switch(msg.getType()) {
+            case PACKET_IN:
+                buffer.add(new SwitchMessagePair(sw, msg));
+                break;
+            default:
+                break;
+        }
+        return Command.CONTINUE;
+    }
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+        Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+        l.add(IFloodlightProviderService.class);
+        return l;
+    }
+
+    @Override
+    public void init(FloodlightModuleContext context)
+            throws FloodlightModuleException {
+        floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+    }
+
+    @Override
+    public void startUp(FloodlightModuleContext context) {
+        floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+    }
+}
